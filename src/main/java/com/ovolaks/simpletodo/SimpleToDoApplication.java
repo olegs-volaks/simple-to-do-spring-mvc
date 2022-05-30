e3b79733382b3475dead34702e6021f78b1585c6
Olegs Volaks <olegs.volaks@gmail.com>
2022-05-30 23:57:21 +0300

    init

---
+++ src/main/java/com/ovolaks/simpletodo/SimpleToDoApplication.java
@@ -0,0 +1,13 @@
+package com.ovolaks.simpletodo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleToDoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimpleToDoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ovolaks/simpletodo/SimpleToDoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ovolaks.simpletodo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleToDoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
