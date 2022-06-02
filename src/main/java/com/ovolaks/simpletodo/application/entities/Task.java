574071bdb3be8ded5a8f85fcbb460a5bb49b3a5a
Olegs Volaks <olegs.volaks@gmail.com>
2022-06-02 05:35:02 +0300

    Added task model

---
+++ src/main/java/com/ovolaks/simpletodo/application/entities/Task.java
@@ -0,0 +1,47 @@
+package com.ovolaks.simpletodo.application.entities;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+import org.hibernate.Hibernate;
+import org.hibernate.annotations.Type;
+
+import javax.persistence.*;
+import java.util.Objects;
+
+@Entity
+@Table(name = "tasks")
+@Getter
+@Setter
+@ToString
+@RequiredArgsConstructor
+public class Task {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "description")
+    @Type(type = "text")
+    private String description;
+
+    @Column(name = "completed")
+    private boolean completed;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || Hibernate.getClass(this) != Hibernate.getClass(o)) return false;
+        Task task = (Task) o;
+        return id != null && Objects.equals(id, task.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return getClass().hashCode();
+    }
+}
