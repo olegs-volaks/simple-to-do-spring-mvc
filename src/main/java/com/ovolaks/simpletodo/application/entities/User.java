bb6c3b11ba089330d6307c3144d5b1de5c43833c
Olegs Volaks <olegs.volaks@gmail.com>
2022-06-03 11:16:39 +0300

    Added username uniq

--- src/main/java/com/ovolaks/simpletodo/application/entities/User.java
+++ src/main/java/com/ovolaks/simpletodo/application/entities/User.java
@@ -26,7 +26,7 @@ public class User implements UserDetails {
     @Column(name = "id", nullable = false)
     private Long id;
 
-    @Column(name = "username")
+    @Column(name = "username", unique = true)
     private String username;
 
     @Column(name = "password")
